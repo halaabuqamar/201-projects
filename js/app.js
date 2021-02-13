e4b8e022bb3d1af03901d5e659fcdb6a057aef5c
halaabuqamar <halaabuqamar.1994@hotmail.com>
2021-02-13 21:54:26 +0200

    finalsite

--- js/app.js
+++ js/app.js
@@ -15,11 +15,11 @@ ques7();
 
 function ques1(){
   let q1 = prompt('is my name hala?');
-  if (q1.toLowerCase() ==='yes' || q1 === 'y') {
+  if (q1.toLowerCase() ==='yes' || q1.toLocaleLowerCase() === 'y') {
     //console.log('that\'s correct');
     alert('that\'s correct');
     grade+=1;
-  } else if (q1 === 'no' || q1 === 'n') {
+  } else if (q1.toLocaleLowerCase() === 'no' || q1.toLocaleLowerCase() === 'n') {
     alert('wrong answer');
   } else {
     alert('please answer yes or no');
@@ -31,11 +31,11 @@ function ques1(){
 
 function ques2(){
   let q2 = prompt('Am i from lebanon?');
-  if (q2.toLowerCase() === 'no' || q2 === 'n') {
+  if (q2.toLowerCase() === 'no' || q2.toLocaleLowerCase() === 'n') {
   //console.log('wrong answer');
     alert('correct');
     grade+=1;
-  } else if (q2 === 'yes' || q2 === 'y') {
+  } else if (q2.toLocaleLowerCase() === 'yes' || q2.toLocaleLowerCase() === 'y') {
     alert('wrong');
   }else{
     alert('please answer yes or no');
@@ -47,12 +47,12 @@ function ques2(){
 
 function ques3(){
   let q3 = prompt ('im i pharmacist');
-  if (q3.toLowerCase() === 'yes' || q3 === 'y') {
+  if (q3.toLowerCase() === 'yes' || q3.toLocaleLowerCase() === 'y') {
   //console.log('that\'s correct')
     alert('that\'s correct');
     grade+=1;
 
-  } else if (q3 === 'no' || q3 === 'n'){
+  } else if (q3.toLocaleLowerCase() === 'no' || q3.toLocaleLowerCase() === 'n'){
     alert('wrong answer');
   } else {
     alert('please answer yes or no');
@@ -64,11 +64,11 @@ function ques3(){
 
 function ques4(){
   let q4 = prompt('Did i work at a hospital?');
-  if (q4.toLowerCase() === 'no' || q4 === 'n') {
+  if (q4.toLowerCase() === 'no' || q4.toLocaleLowerCase() === 'n') {
   //console.log('wrong answer');
     alert('correct,good job');
     grade+=1;
-  } else if (q4 === 'yes' || q4 === 'y') {
+  } else if (q4.toLocaleLowerCase() === 'yes' || q4.toLocaleLowerCase() === 'y') {
     alert('wrong answer :(');
   } else{
     alert('please answer yes or no');
@@ -79,11 +79,11 @@ function ques4(){
 
 function ques5(){
   let q5 = prompt('Do i like being an employee?');
-  if (q5.toLowerCase() === 'no' || q5=== 'n') {
+  if (q5.toLowerCase() === 'no' || q5.toLocaleLowerCase()=== 'n') {
   //console.log('wrong answer');
     alert('correct,this was the last question');
     grade+=1;
-  } else if (q5 === 'yes' || q5 === 'y') {
+  } else if (q5.toLocaleLowerCase() === 'yes' || q5.toLocaleLowerCase() === 'y') {
     alert('wrong answer,goodluck next time');
   } else{
     alert('Next time please answer yes or no');
@@ -117,27 +117,19 @@ function ques6(){
 //q7
 function ques7(){
   let places=['scotland','india','moroco','north pole','kenya','boston-usa','spain','philippines','japan','russia'];
-  let flag=true;
-  let answer=prompt('can you guess what country i would like to visit the most?');
+  let answer=prompt('can you guess one country from my (would like to visit)list?');
   var atmp=5;
-  while (flag){
+ myloop: while (atmp){
+    atmp=atmp-1;
     for(let j=0;j<places.length;j++){
-      if (atmp===0) {
-        flag=false;
-        break;
-      }
       if(answer.toLowerCase() === places[j]){
         console.log('answer is'+answer);
         alert('correct');
         grade+=1;
-        flag=false;
-        break ;
-      }
-      else{
-        answer =prompt('try again you have '+ atmp+' left');
-        atmp=atmp-1;
+        break myloop;
       }
     }
+    answer =prompt('try again you have '+ atmp+' left');
   }
 }
 
